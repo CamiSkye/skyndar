4968be8076f2541af6148af6d25956bf3f5c6e89
Camille <clacroix@et.esiea.fr>
2025-07-07 11:47:56 +0200

    Refresh page

--- Web/Views/Prestation.php
+++ Web/Views/Prestation.php
@@ -5,10 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-
     <link rel="stylesheet" href="../Styles/prestation.css">
-    
-    <title>Document</title>
+
+    <script>
+        setInterval(() => 
+        {
+            location.reload(); 
+        }, 5000); 
+    </script>
+    <title>Prestation</title>
 </head>
 <body>
 <header class="header">
